fe55c233edffb56cc27da8bdfe014013364299a4
Aliaksei Loi <alex.loi@pandadoc.com>
2021-08-09 12:55:37 +0300

    add animation

--- modules/main/index.tsx
+++ modules/main/index.tsx
@@ -1,33 +1,40 @@
 import { useTimer } from '../../hooks/useTimer'
 
 import styles from './index.module.scss'
+import { useSpring, animated, config } from 'react-spring'
+
+const commonSpring = {
+    from: { opacity: 0, color: '#ffefd5', scale: 0.95 },
+    to: { opacity: 1, color: '#666', scale: 1 },
+    config: config.molasses,
+}
 
 export default function Home() {
-    const [{ days, hours, minutes, seconds }] = useTimer()
+    const [date] = useTimer()
+
+    const titleStyles = useSpring({
+        ...commonSpring,
+    })
+    const dateStyles = useSpring({
+        ...commonSpring,
+        delay: 200,
+    })
+
+    const items = Object.entries(date).map(([key, value]) => ({ key, value }))
 
     return (
         <div className={styles.container}>
-            <h1 className={styles.h1} id="headline">
+            <animated.h1 className={styles.h1} style={titleStyles}>
                 Countdown to our wedding:
-            </h1>
-            <ul className={styles.ul}>
-                <li className={styles.li}>
-                    <span className={styles.span}>{days}</span>
-                    days
-                </li>
-                <li className={styles.li}>
-                    <span className={styles.span}>{hours}</span>
-                    Hours
-                </li>
-                <li className={styles.li}>
-                    <span className={styles.span}>{minutes}</span>
-                    Minutes
-                </li>
-                <li className={styles.li}>
-                    <span className={styles.span}>{seconds}</span>
-                    Seconds
-                </li>
-            </ul>
+            </animated.h1>
+            <animated.ul className={styles.ul} style={dateStyles}>
+                {items.map(({ key, value }) => (
+                    <li className={styles.li} key={key}>
+                        <span className={styles.span}>{value}</span>
+                        {key}
+                    </li>
+                ))}
+            </animated.ul>
         </div>
     )
 }
